f8f16341cc5685efae8942c7a52970a85f6f7371
David León <108154987+DvLeu@users.noreply.github.com>
2022-09-03 14:55:54 -0500

    Update 11172.cpp

--- 11172.cpp
+++ 11172.cpp
@@ -1,17 +1,1 @@
-#include <stdio.h>
 
-int main()
-{
-    long long ally ;
-    long long enemy;
-    long long rest;
-    
-    if(a > b){
-
-    cout << a - b;
-    }else{
-    cout << b - a;
-    }
-    
-    return 0;
-}
