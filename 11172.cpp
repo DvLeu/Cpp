ed02d9076bc8ac3c2aa2844131a303483822134c
David León <108154987+DvLeu@users.noreply.github.com>
2022-09-03 14:56:58 -0500

    Update 11172.cpp

--- 11172.cpp
+++ 11172.cpp
@@ -1,1 +1,18 @@
-
+#include <stdio.h>
+#include <iostream>
+int main()
+{
+    int a,b;
+    std::cin>>a;
+    std::cin>>b;
+    if(a>b){
+      std::cout<<">"<<std::endl;
+    }else if(a<b){
+      std::cout<<"<"<<std::endl;
+    }else{
+      std::cout<<"="<<std::endl;
+    }
+  };
+}
+    return 0;
+}
